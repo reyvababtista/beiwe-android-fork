2c163062d5a8748d289bad87528e45c07a33feee
Josh Zagorsky <josh@zagaran.com>
2021-08-23 18:47:37 -0400

    Show message in MessageActivity

--- app/src/main/java/org/beiwe/app/ui/MessageActivity.kt
+++ app/src/main/java/org/beiwe/app/ui/MessageActivity.kt
@@ -1,14 +1,20 @@
 package org.beiwe.app.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_message.*
 import org.beiwe.app.R
 
 class MessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message)
-        Log.e("JoshLog", "Opening message activity!")
+        val messageContent = intent.getStringExtra("messageContent")
+
+        if (messageContent != null) {
+            messageScreenTextView.text = messageContent
+        }
     }
 }
